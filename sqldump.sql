c4d23eba1767dd4f30618b0407d7c7ad8ff3d804
Prashmit Tanay <tanay.wsd@nic.in>
2013-10-03 18:25:18 +0530

    sqldump modified

--- sqldump.sql
+++ sqldump.sql
@@ -17,13 +17,11 @@ USE `learninghibernate`;
 /*Table structure for table `events` */
 
 CREATE TABLE `events` (
-  `event_id` int(5) NOT NULL,
+  `event_id` int(5) NOT NULL AUTO_INCREMENT,
   `event_date` datetime DEFAULT NULL,
   `title` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`event_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*Data for the table `events` */
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
