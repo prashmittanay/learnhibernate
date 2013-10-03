2e59cac285e0768c2b8847f564c458a1c44176e9
Prashmit Tanay <tanay.wsd@nic.in>
2013-10-03 17:54:45 +0530

    HibernateUtil contructed

--- src/main/java/learn/hibernate/test/HibernateUtil.java
+++ src/main/java/learn/hibernate/test/HibernateUtil.java
@@ -1,5 +1,26 @@
 package learn.hibernate.test;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
-	
+	// session factory is thread safe
+	// instantiate only one per application life-cycle
+	// used to retrieve individual Session objects
+	private static final SessionFactory SESSION_FACTORY = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+			// create the SessionFactory from hibernate.cfg.xml
+			return new Configuration().configure().buildSessionFactory();
+		} catch (Throwable ex) {
+			// Make sure you log the exception, as it might be swallowed
+			System.err.println("Initial SessionFactory creation failed: " + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return SESSION_FACTORY;
+	}
 }
